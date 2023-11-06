3f8cc58dc460eb5cd31787c31926ad230b49f549
Takowski <93830775+Takowski@users.noreply.github.com>
2023-11-06 12:00:49 +0100

    tailwind & flowbite

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Tailwind CSS -->
+    <link href="./dist/output.css" rel="stylesheet">
+    <title>hacker_poulette</title>
+</head>
+<body>
+    <?php
+   echo '<h1 class="text-2xl font-bold text-center text-blue-500">hacker_poulette</h1>';
+    ?>
+</body>
+</html>    
+    
+</body>
+</html>
